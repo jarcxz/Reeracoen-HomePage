19c30177e9d814f5a4cbc3fc15ca30f57965c9c5
jarcxz <51941123+jarcxz@users.noreply.github.com>
2019-06-18 15:08:56 +0800

    Add Files via upload
    Update Web Page

---
+++ js/script.js
@@ -0,0 +1,99 @@
+/*
+
+
+// Copyright 2019 The Closure Library Authors. All Rights Reserved.
+//
+// Licensed under the JarvisPaladaWEB, Version 1.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      jarcxzit@gmail.com
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//DEVELOP BY JARVIS CHRISTIAN S. PALADA
+
+
+*/
+
+/*
+
+
+// Copyright 2019 The Closure Library Authors. All Rights Reserved.
+//
+// Licensed under the JarvisPaladaWEB, Version 1.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      jarcxzit@gmail.com
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//DEVELOP BY JARVIS CHRISTIAN S. PALADA
+
+
+*/
+
+
+var date = new Date();
+var hours = date.getHours();
+var greet;
+
+if(hours < 12){
+	greet = "<center><img src='images/image1.png' width='500'/></center>";
+}else if(hours >= 12 && hours <= 17){
+	greet = "<center><img src='images/image2.png' width='500'/></center>";
+}else if(hours >= 17 && hours <= 24){
+	greet = "<center><img src='images/image3.png' width='500'/></center>";
+}
+
+document.getElementById('greeting').innerHTML = greet;
+
+
+/*
+
+
+// Copyright 2019 The Closure Library Authors. All Rights Reserved.
+//
+// Licensed under the JarvisPaladaWEB, Version 1.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      jarcxzit@gmail.com
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//DEVELOP BY JARVIS CHRISTIAN S. PALADA
+
+
+*/
+
+/*
+
+
+// Copyright 2019 The Closure Library Authors. All Rights Reserved.
+//
+// Licensed under the JarvisPaladaWEB, Version 1.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      jarcxzit@gmail.com
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//DEVELOP BY JARVIS CHRISTIAN S. PALADA
+
+
+*/
